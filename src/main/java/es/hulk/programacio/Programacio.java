7b2843c5c1ce27f6b08fd25ba8d03b8d647c3c38
Miquel Angel <hulkrojo2@gmail.com>
2021-09-29 10:32:25 +0200

    Removed useless code

--- src/main/java/es/hulk/programacio/Programacio.java
+++ src/main/java/es/hulk/programacio/Programacio.java
@@ -1,7 +1,5 @@
 package es.hulk.programacio;
 
-import java.util.Scanner;
-
 /**
  * Created by Hulk
  * at 27/09/2021 9:52
@@ -9,21 +7,7 @@ import java.util.Scanner;
 
 public class Programacio {
 
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        System.out.println(suma());
-    }
-
-    public static void scanner() {
-        String scan = scanner.nextLine();
-        System.out.println(scan);
-    }
-
-    public static int suma() {
-        int num = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        return num + num2;
     }
 
 }
