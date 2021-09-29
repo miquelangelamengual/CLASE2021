aca3d807c829bdd15898e60eb399834e2fe4ac97
Miquel Angel <hulkrojo2@gmail.com>
2021-09-29 10:30:11 +0200

    test webhook

--- src/main/java/es/hulk/programacio/Programacio.java
+++ src/main/java/es/hulk/programacio/Programacio.java
@@ -1,5 +1,7 @@
 package es.hulk.programacio;
 
+import java.util.Scanner;
+
 /**
  * Created by Hulk
  * at 27/09/2021 9:52
@@ -7,6 +9,21 @@ package es.hulk.programacio;
 
 public class Programacio {
 
-    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println(suma());
+    }
+
+    public static void scanner() {
+        String scan = scanner.nextLine();
+        System.out.println(scan);
+    }
+
+    public static int suma() {
+        int num = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        return num + num2;
+    }
 
 }
