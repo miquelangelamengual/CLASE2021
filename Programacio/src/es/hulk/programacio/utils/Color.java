b357eb076979a1ab20344223c7d71dcf97a78b8c
miquelangelamengual <hulkrojo2@gmail.com>
2021-11-03 13:11:53 +0100

    Changed state of colors to private

--- Programacio/src/es/hulk/programacio/utils/Color.java
+++ Programacio/src/es/hulk/programacio/utils/Color.java
@@ -1,22 +1,20 @@
 package es.hulk.programacio.utils;
 
-import lombok.Getter;
 import lombok.experimental.UtilityClass;
 
-@Getter
 @UtilityClass
 public class Color {
 
-    public static final String reset = "\u001B[0m";
+    private static final String reset = "\u001B[0m";
 
-    public static final String black = "\u001B[30m";
-    public static final String red = "\u001B[31m";
-    public static final String green = "\u001B[32m";
-    public static final String yellow = "\u001B[33m";
-    public static final String blue = "\u001B[34m";
-    public static final String purple = "\u001B[35m";
-    public static final String cyan = "\u001B[36m";
-    public static final String white = "\u001B[37m";
+    private static final String black = "\u001B[30m";
+    private static final String red = "\u001B[31m";
+    private static final String green = "\u001B[32m";
+    private static final String yellow = "\u001B[33m";
+    private static final String blue = "\u001B[34m";
+    private static final String purple = "\u001B[35m";
+    private static final String cyan = "\u001B[36m";
+    private static final String white = "\u001B[37m";
 
     public static void printColor(String mode, String color, String text) {
         if (mode.equals("newline")) {
