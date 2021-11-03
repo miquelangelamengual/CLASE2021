3c86fba4cb93f9052ed44e25d1c7174be73b5b4c
miquelangelamengual <hulkrojo2@gmail.com>
2021-11-03 13:11:41 +0100

    Added more Loop Exercises

--- Programacio/src/es/hulk/programacio/exercises/LoopsExercises.java
+++ Programacio/src/es/hulk/programacio/exercises/LoopsExercises.java
@@ -3,7 +3,6 @@ package es.hulk.programacio.exercises;
 import es.hulk.programacio.utils.Color;
 import es.hulk.programacio.utils.Utils;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -16,16 +15,11 @@ public class LoopsExercises {
     private Scanner scanner = new Scanner(System.in);
 
     public void printNumbers() {
-        try {
-            int x = scanner.nextInt();
+        int x = scanner.nextInt();
 
-            for (int i = 0; i <= x; i++) {
-                Color.printColor("newline", "cyan", " [" + i + "] ");
-            }
-        } catch (Exception e) {
-            Color.printColor("newline", "red", "Oh oh, i think i broke something.......");
+        for (int i = 0; i <= x; i++) {
+            Color.printColor("newline", "cyan", " [" + i + "] ");
         }
-
     }
 
     public void ifNegativeStop() {
@@ -50,6 +44,69 @@ public class LoopsExercises {
             Utils.numberPositiveOrNegative(x);
         }
         Color.printColor("newline", "red", "Number 0, stoping the loop....");
+    }
+
+    public void ifMultipleOf2Stop() {
+        Color.printColor("line", "purple", "Tellme a number: ");
+        int x = scanner.nextInt();
+
+        while (x % 2 != 0) {
+            x = scanner.nextInt();
+        }
+        Color.printColor("newline", "red", "Multiple of 2, stoping the loop....");
+    }
+
+    public void saveNumberIfNumberNegative() {
+        int counter = 0;
+        Color.printColor("line", "purple", "First Number: ");
+        int x = scanner.nextInt();
+        Color.printColor("newline", "green", "Number is " + x);
+
+        while (x > 0) {
+            x = scanner.nextInt();
+            Color.printColor("newline", "green", "Number is " + x);
+            counter++;
+        }
+        Color.printColor("newline", "red", "Negative number, stoping the loop...");
+        Color.printColor("newline", "blue", "Total counted numbers: " + counter);
+    }
+
+    public void sumNumberIfNumberNegative() {
+        int total = 0;
+        Color.printColor("line", "purple", "First Number: ");
+        int x = scanner.nextInt();
+        total += x;
+
+        while (x > 0) {
+            x = scanner.nextInt();
+            if (x < 0) {
+                break;
+            }
+            total += x;
+
+        }
+        Color.printColor("newline", "red", "Negative number, stoping the loop...");
+        Color.printColor("newline", "blue", "Result: " + total);
+
+    }
+
+    public void calculAveraged() {
+        int total = 0;
+        Color.printColor("line", "purple", "First Number: ");
+        int x = scanner.nextInt();
+        int counter = 1;
+        total += x;
+
+        while (x > 0) {
+            x = scanner.nextInt();
+            if (x < 0) {
+                break;
+            }
+            total += x;
+            counter++;
+        }
+        Color.printColor("newline", "red", "Negative number, stoping the loop...");
+        Color.printColor("newline", "blue", "Result: " + (double) total / counter);
     }
 
 }
