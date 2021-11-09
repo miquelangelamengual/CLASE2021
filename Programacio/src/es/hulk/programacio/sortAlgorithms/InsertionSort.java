de5d5a433feef5c9b8905f5f4ce67260506c39be
miquelangelamengual <hulkrojo2@gmail.com>
2021-11-09 08:26:06 +0000

    idc

---
+++ Programacio/src/es/hulk/programacio/sortAlgorithms/InsertionSort.java
@@ -0,0 +1,14 @@
+package es.hulk.programacio.sortAlgorithms;
+
+/**
+ * Created by Hulk
+ * at 8/11/21 12:29
+ */
+
+public class InsertionSort {
+
+    public static void applySort(int[] arr, String mode) {
+
+    }
+
+}
