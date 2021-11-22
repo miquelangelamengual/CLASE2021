755e3fafb9d16118a40cf2d8a1ed0e33f9d6fa30
Miquel Angel Amengual Sastre <miquelangel2309@gmail.com>
2021-11-22 11:44:49 +0100

    Added practice of TallerB

--- Programacio/src/es/hulk/programacio/practiques/tallerB/Cinema.java
+++ Programacio/src/es/hulk/programacio/practiques/tallerB/Cinema.java
@@ -1,8 +1,28 @@
 package es.hulk.programacio.practiques.tallerB;
+
 /**
  * Created by Hulk
  * at 22/11/2021 11:39
  */
 
+
 public class Cinema {
+
+    private String nom;
+    private int numSala;
+    private Pelicula[] pelicules;
+
+    public Cinema(String nom, int numSala) {
+        this.nom = nom;
+        this.numSala = numSala;
+    }
+
+    public Pelicula peliculaMesLlarga() {
+        for (Pelicula pelicula : pelicules) {
+            if (pelicula.getDurada() > pelicula.getDurada()) {
+                return pelicula;
+            }
+        }
+        return null;
+    }
 }
--- Programacio/src/es/hulk/programacio/practiques/tallerB/Pelicula.java
+++ Programacio/src/es/hulk/programacio/practiques/tallerB/Pelicula.java
@@ -1,8 +1,25 @@
 package es.hulk.programacio.practiques.tallerB;
+
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by Hulk
  * at 22/11/2021 11:39
  */
 
+@Getter
+@Setter
 public class Pelicula {
+
+    private String titol;
+    private String director;
+    private int durada;
+
+    public Pelicula(String titol, String director, int durada) {
+        this.titol = titol;
+        this.director = director;
+        this.durada = durada;
+    }
+
 }
