bc5e7a73d78acd4c054bafdf7a7024e0817fa112
Miquel Angel Amengual Sastre <miquelangel2309@gmail.com>
2021-11-22 11:58:28 +0100

    Added exercice to practice contructors, getters and setters

---
+++ Programacio/src/es/hulk/programacio/practiques/practica/Alumne.java
@@ -0,0 +1,38 @@
+package es.hulk.programacio.practiques.practica;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Hulk
+ * at 22/11/2021 11:48
+ */
+
+@Getter
+@Setter
+public class Alumne {
+
+    private String id;
+    private String nom;
+    private int edat;
+    private int[] notas;
+
+    public Alumne(String id, String nom, int edat, int[] notas) {
+        this.id = id;
+        this.nom = nom;
+        this.edat = edat;
+        this.notas = notas;
+    }
+
+    public Alumne() {
+
+    }
+
+    public void mitjanaNotes() {
+        int suma = 0;
+        for (int i = 0; i < notas.length; i++) {
+            suma += notas[i];
+        }
+        System.out.println("La mitjana de les notes de l'assignatura " + nom + " és: " + (suma / notas.length));
+    }
+}
---
+++ Programacio/src/es/hulk/programacio/practiques/practica/Assignatura.java
@@ -0,0 +1,47 @@
+package es.hulk.programacio.practiques.practica;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Hulk
+ * at 22/11/2021 11:49
+ */
+
+@Getter
+@Setter
+public class Assignatura {
+
+    private String nom;
+    ArrayList<Alumne> alumnes;
+
+    public Assignatura(String nom) {
+        this.nom = nom;
+        this.alumnes = new ArrayList<>();
+    }
+
+    public void nouMatriculat(Alumne alumne) {
+        alumnes.add(alumne);
+    }
+
+
+    public void baixaMatriculat(Alumne alumne) {
+        alumnes.remove(alumne);
+    }
+
+    public Alumne millorAlumne() {
+        int max = 0;
+        Alumne alumne = null;
+        for (Alumne a : alumnes) {
+            for (int i = 0; i < a.getNotas().length; i++) {
+            if (alumne.getNotas()[i] > max) {
+                    max = a.getNotas()[i];
+                    alumne = a;
+                }
+            }
+        }
+        return alumne;
+    }
+}
