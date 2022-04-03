53373c1fcb7fb7c5edfef318730cf54bd5ed7359
Miquel Angel <hulkrojo2@gmail.com>
2022-04-03 15:07:20 +0200

    added max mines and max flags

--- Buscaminas/src/main/java/es/hulk/buscaminas/objects/Box.java
+++ Buscaminas/src/main/java/es/hulk/buscaminas/objects/Box.java
@@ -7,21 +7,23 @@ import lombok.Setter;
 public class Box {
 
     private boolean flag;
+    private int numFlags;
     private boolean mine;
     private boolean open;
     private int minesAround;
+    private int mines;
     private int x;
     private int y;
 
-    public Box(int x, int y) {
+    public Box(int x, int y, int mines, int numFlags) {
         this.x = x;
         this.y = y;
-        this.flag = false;
         this.mine = false;
         this.open = false;
+        this.flag = false;
         this.minesAround = 0;
+        this.mines = mines;
+        this.numFlags = numFlags;
     }
-
-
 
 }
