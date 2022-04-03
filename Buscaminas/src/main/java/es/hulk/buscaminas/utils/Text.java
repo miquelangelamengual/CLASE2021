2c03b30bf8e41808fb082f33ca6122a64dd2f170
Miquel Angel <hulkrojo2@gmail.com>
2022-04-03 15:08:28 +0200

    added experimental class where all text will be stored in order to remove text redundancy

---
+++ Buscaminas/src/main/java/es/hulk/buscaminas/utils/Text.java
@@ -0,0 +1,14 @@
+package es.hulk.buscaminas.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Text {
+
+    public String REMOVE_FLAG = "Has quitado una bandera";
+    public String PUT_FLAG = "Has puesto una bandera";
+    public String WIN = "Has ganado";
+    public String LOSE = "Has perdido";
+    public String FLAGS_REMAINING = "Banderas restantes ";
+
+}
