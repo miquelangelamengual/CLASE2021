e629a3ce899ddd29378c1324e8a88a92708ac442
Miquel Angel <hulkrojo2@gmail.com>
2022-04-07 09:48:42 +0200

    added options to left game and reset the game

--- Buscaminas/src/main/java/es/hulk/buscaminas/menus/ElectionMenu.java
+++ Buscaminas/src/main/java/es/hulk/buscaminas/menus/ElectionMenu.java
@@ -2,6 +2,7 @@ package es.hulk.buscaminas.menus;
 
 import es.hulk.buscaminas.Buscaminas;
 import es.hulk.buscaminas.objects.Box;
+import es.hulk.buscaminas.utils.ResetGame;
 import es.hulk.buscaminas.utils.Text;
 import es.hulk.buscaminas.utils.Utilities;
 import lombok.NoArgsConstructor;
@@ -12,33 +13,34 @@ public class ElectionMenu {
     private MainMenu menu = Buscaminas.getMenu();
     private Box[][] box = Buscaminas.getMenu().getBoard().getBoard();
 
+    private int x;
+    private int y;
+
     public void electionMenu() {
         Text.printElectionMenu();
         int option = Utilities.readInt();
 
-        if (option == 4) {
-            System.exit(404);
-            return;
-        }
-
-        Utilities.log(Text.CUSTOM_BOARD_ROWS);
-        int x = Utilities.readInt();
-
-        Utilities.log(Text.CUSTOM_BOARD_COLS);
-        int y = Utilities.readInt();
-
         switch (option) {
             case 1:
+                this.askNumbers(x, y);
                 box[x][y].openBox();
                 this.printBoardAndElectionMenu();
                 break;
             case 2:
+                this.askNumbers(x, y);
                 box[x][y].putFlag();
                 this.printBoardAndElectionMenu();
                 break;
             case 3:
+                this.askNumbers(x, y);
                 box[x][y].removeFlag();
                 this.printBoardAndElectionMenu();
+                break;
+            case 4:
+                ResetGame.askResetGame();
+                break;
+            case 5:
+                System.exit(404);
                 break;
         }
     }
@@ -46,5 +48,13 @@ public class ElectionMenu {
     public void printBoardAndElectionMenu() {
         menu.getBoard().printBoard();
         this.electionMenu();
+    }
+
+    private void askNumbers(int x, int y) {
+        Utilities.log(Text.CUSTOM_BOARD_ROWS);
+        x = Utilities.readInt();
+
+        Utilities.log(Text.CUSTOM_BOARD_COLS);
+        y = Utilities.readInt();
     }
 }
